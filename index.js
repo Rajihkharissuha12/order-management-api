f65088acbe9681e91f7bb08ffb9ec376dcd4dec1
Rajih kharissuha <rajhk704@gmail.com>
2025-08-15 21:09:07 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
